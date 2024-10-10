21f1949911c36d23b2b9a519ffca9bfbfd566b1a
Sanyog Dani <113448326+SanD204@users.noreply.github.com>
2024-10-10 11:44:55 +0530

    Initial commit

---
+++ NoteShare/NoteShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteShareApp.swift
+//  NoteShare
+//
+//  Created by admin24 on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NoteShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
